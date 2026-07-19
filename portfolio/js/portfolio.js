5a55d1de5553ccae72ed2a3af694d0aed1be842d
Tomas Misura <tmisura@gmail.com>
2026-07-19 15:22:15 +0200

    bugfix duplicity

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -544,7 +544,6 @@ function GetTickers(letter = ''){
 
 function GetCryptos(letter = ''){
     const xhttp = new XMLHttpRequest(); 
-   const xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("tickerDetailsContent").innerHTML = this.responseText;
