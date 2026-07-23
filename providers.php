9e5eba556e053d04f61f389ac6979af8602d82aa
Tomas Misura <tmisura@gmail.com>
2026-07-23 19:18:32 +0200

    Add Poznámky link to providers.php nav

--- providers.php
+++ providers.php
@@ -35,10 +35,11 @@
                         <li><a href="<?= $base ?>index.php"><i class="fas fa-home"></i> Domov</a></li>
                         <li><a href="<?= $base ?>/portfolio/index.php"><i class="fas fa-chart-line"></i> Portfólio</a></li>
                         <li><a href="<?= $base ?>providers.php"><i class="fas fa-building"></i> Poskytovatelia</a></li>
+                         <li><a href="<?= $base ?>notes.php"><i class="fas fa-sticky-note"></i> Poznámky</a></li>
                        <!--  <li><a href="<?= $base ?>influencers.php"><i class="fas fa-users"></i> Influencers</a></li>
                         <li><a href="<?= $base ?>news.php"><i class="fas fa-newspaper"></i> Novinky</a></li>
                         <li><a href="<?= $base ?>settings.php"><i class="fas fa-cogs"></i> Nastavenia</a></li>
-                        <li><a href="<?= $base ?>notes.php"><i class="fas fa-sticky-note"></i> Poznámky</a></li> -->
+                        -->
                         <li><a href="<?= $base ?>logout.php"><i class="fas fa-sign-out-alt"></i> Odhlásiť sa</a></li>
                     </ul>
                 </nav>
